1f5eba4743c23cd372700917ce548c23404b82bf
Filip Chyla <filip@chy.la>
2022-08-21 10:54:10 +0200

    update script

--- update_dns.sh
+++ update_dns.sh
@@ -4,16 +4,14 @@ source ../cloudflare.vars
 
 WAN_IP=`curl ifconfig.co`
 CF_RECORD_IP=`curl -sS -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-     -H "X-Auth-Email: $CF_EMAIL" \
-     -H "X-Auth-Key: $CF_API_KEY" \
-     -H "Content-Type: application/json" | jq -r '.[].content' 2>/dev/null`
-
+	-H "Authorization: Bearer $CF_API_KEY" \
+	-H "Content-Type: application/json" | jq -r '.[].content' 2>/dev/null`
+echo $CF_RECORD_IP
 if [[ $CF_RECORD_IP != $WAN_IP ]]; then
   logger "Changing VPN DNS record to $WAN_IP"
   #Change IP via cloudflare api
   curl -sS -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
-     -H "X-Auth-Email: $CF_EMAIL" \
-     -H "X-Auth-Key: $CF_API_KEY" \
+	 -H "Authorization: Bearer $CF_API_KEY" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'$DNS_RECORD'","content":"'$WAN_IP'","proxied":false}'
   #Notify via pushbullet
